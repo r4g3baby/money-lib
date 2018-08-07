213d81cbc5f4c25c6c1cb0a0f7b7848a7a5a450c
Rui Pereira <r4g3baby@gmail.com>
2018-08-07 01:59:54 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+
+from setuptools import setup, find_packages
+
+with open('money/__init__.py', 'r', encoding='utf-8') as init:
+    version = re.search('__version__ = [\'"]([^\'"]+)[\'"]', init.read()).group(1)
+
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='money-class',
+    version=version,
+    author='Rui Pereira',
+    author_email='r4g3baby@gmail.com',
+    license='MIT',
+    description='Python 3 money class with decimal precision and currency exchange support.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/R4G3_BABY/money-class',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['Babel>=2.5.0'],
+    python_requires='>=3',
+    classifiers=(
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    )
+)
