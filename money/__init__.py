81f70d434bb0b6fe7e41c4e4af7761759f16eb04
Rui Pereira <r4g3baby@gmail.com>
2020-10-08 14:19:57 +0100

    Fix flake8 linting errors

--- money/__init__.py
+++ money/__init__.py
@@ -1,6 +1,8 @@
-__name__ = 'money'
-__version__ = '2.1.0'
-
 from .currency import Currency
 from .exchange import xrates
 from .money import Money
+
+__name__ = 'money'
+__version__ = '2.1.0'
+
+__all__ = ('Money', 'Currency', 'xrates')
