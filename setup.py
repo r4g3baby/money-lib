eda9bd10dd54422cc00adb851407d3cdba4acfff
Rui Pereira <r4g3baby@gmail.com>
2020-10-08 14:35:36 +0100

    Add python 3.8 and some other classifiers

--- setup.py
+++ setup.py
@@ -25,12 +25,15 @@ setup(
     },
     python_requires='>=3',
     classifiers=[
-        'Programming Language :: Python :: Implementation :: CPython',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers'
+        'Programming Language :: Python :: 3.8'
     ]
 )
